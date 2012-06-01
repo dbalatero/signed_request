d530dbbd82ba82d303bdb332cbdb640c3799fb69
Rob Hanlon <rob@mediapiston.com>
2012-05-31 22:39:53 -0700

    Make sure signed_request functions the same on Ruby 1.9.x.

--- lib/signed_request.rb
+++ lib/signed_request.rb
@@ -3,41 +3,47 @@ require 'openssl'
 require 'openssl/digest'
 
 module SignedRequest
-  STRIP_PARAMS = ['action', 'controller', 'format']
 
-  # Sign a request on the sending end.
-  def self.sign(params, secret_key, options = {})
-    params = params.dup
+  class << self
+    # Sign a request on the sending end.
+    def sign(params, secret_key, options = {})
+      params = params.dup
 
-    # Flatten any sub-hashes to a single string.
-    params.keys.each do |key|
-      if params[key].is_a?(Hash)
-        params[key] = params[key].sort_by { |k, v| k.to_s.downcase }.to_s
+      # Flatten any sub-hashes to a single string.
+      params.each do |key, value|
+        params[key] = to_flat_string(value) if value.is_a?(Hash)
       end
+
+      string_to_sign = options[:path].to_s + to_flat_string(params)
+
+      digest  = OpenSSL::Digest::Digest.new('sha1')
+      hmac = OpenSSL::HMAC.digest(digest, secret_key, string_to_sign)
+      encoded = Base64.encode64(hmac).chomp
     end
 
-    query   = params.sort_by { |k,v| k.to_s.downcase }
-    string_to_sign = options[:path].to_s + query.to_s
+    # Validate an incoming request on the receiving end.
+    def validate(params, secret_key, sign_options = {})
+      signature = params.delete('signature') || params.delete(:signature)
 
-    digest  = OpenSSL::Digest::Digest.new('sha1')
-    hmac    = OpenSSL::HMAC.digest(digest, secret_key, string_to_sign)
-    encoded = Base64.encode64(hmac).chomp
-  end
+      return false unless signature
 
-  # Validate an incoming request on the receiving end.
-  def self.validate(params, secret_key, sign_options = {})
-    signature = params.delete('signature') || params.delete(:signature)
-    return false if !signature
-
-    strip_keys_from!(params, *STRIP_PARAMS)
-    actual_signature = sign(params, secret_key, sign_options)
-    actual_signature == signature
-  end
+      strip_keys_from!(params, *strip_params)
+      actual_signature = sign(params, secret_key, sign_options)
+      actual_signature == signature
+    end
 
   private
-  def self.strip_keys_from!(hash, *keys)
-    keys.each do |key|
-      hash.delete(key)
+
+    def to_flat_string(hash)
+      hash.sort_by { |k, | k.to_s.downcase }.map(&:join).join
+    end
+
+    def strip_keys_from!(hash, *keys)
+      keys.each { |key| hash.delete(key) }
+    end
+
+    def strip_params
+      @strip_params ||= %w[action controller format]
     end
   end
 end
